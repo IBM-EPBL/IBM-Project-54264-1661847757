5a5d985106ee1696eea0ed7493a841f206da4bdb
493ADITHYA <113785138+493ADITHYA@users.noreply.github.com>
2022-09-25 15:58:36 +0530

    Add files via upload

---
+++ adithya - Copy/list.py
@@ -0,0 +1,19 @@
+# list
+numbers =['23','65','13','56','65']
+#insert 33 at 3th index
+print (numbers)
+#remove
+numbers.remove('23')
+print(numbers)
+#append
+numbers.append('72')
+print(numbers)
+#reverse
+numbers.reverse()
+print (numbers)
+#sort
+numbers.sort()
+print (numbers)
+#pop
+numbers.pop(3)
+print(numbers)
---
+++ adithya - Copy/str.py
@@ -0,0 +1,11 @@
+#concatenate
+a="violet"
+b="blue"
+c=a+b
+print(c)
+#slice
+b="blue"
+print(b[2:5])
+#reverse
+a="violet"[::-1]
+print (a)
